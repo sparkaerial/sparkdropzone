7d4bc06377073e1c91a7f1dafa19ebe84e5a3996
adminsparkaerial <admin@sparkaerial.com>
2015-01-27 16:27:19 -0800

    production db

--- db.js
+++ db.js
@@ -1,3 +1,10 @@
-module.exports = {
-  'url' : 'mongodb://localhost/passport'
+if (process.env.NODE_ENV === 'production') {
+	module.exports = {
+		'url' : 'mongodb://WfdoUmqEeeMM:LUkPfstcYCrl@mongosoup-cont002.mongosoup.de:31604/cc_WfdoUmqEeeMM'
+	}
+}
+else {
+	module.exports = {
+		'url' : 'mongodb://localhost/test'
+	}
 }
